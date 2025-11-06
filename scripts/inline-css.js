b759cac135062d3e7b67a91283331ec3e575aab7
SolRudd <solrudd@hotmail.com>
2025-11-06 19:53:37 +0000

    Clean build: removed inline CSS script, shadow DOM style isolation stable

--- scripts/inline-css.js
+++
@@ -1,5 +0,0 @@
-import fs from "fs";
-const js = fs.readFileSync("dist/lovecookies.umd.js", "utf8");
-const css = fs.readFileSync("dist/sdk.css", "utf8");
-const inline = `(()=>{const s=document.createElement('style');s.textContent=\`${css}\`;document.head.appendChild(s);})();`;
-fs.writeFileSync("dist/lovecookies.umd.js", inline + "\n" + js);
